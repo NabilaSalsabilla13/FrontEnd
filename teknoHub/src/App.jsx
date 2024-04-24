1c30dfef4621edf854fa11cb14bf3e3673907eb5
Nabila Salsabilla <nabilasalsabilla1312@gmail.com>
2024-04-24 22:29:59 +0700

    Pembuatan Project Navbar &  Layout

---
+++ teknoHub/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="bg-yellow-300">
+    <ul className="flex gap-6 justify-center">
+    <li>hai</li>
+    <li>hai</li>
+    <li>hai</li>
+    </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
